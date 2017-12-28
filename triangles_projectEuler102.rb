ca39141b59c9d3255bc6d40b579542dedbe79799
Jon Zingale <jonzingale@gmail.com>
2017-12-28 13:54:22 -0700

    visualizer for triangles

--- triangles_projectEuler102.rb
+++ triangles_projectEuler102.rb
@@ -1,20 +1,43 @@
-  def settings
-    size(displayWidth, displayHeight)
-  end
+require 'matrix'
 
-	def setup
-		@w, @h = [width/2.0, 0]
-		@i = 0 ; @t = 0
-    frame_rate 20
+A = Vector.elements([-1, -3])
+B = Vector.elements([-3, 3])
+C = Vector.elements([4, 1])
 
-		colorMode(HSB,360,100,100,100)
-		background(0)
+BA, CA = B - A, C - A
+
+def settings
+  size(displayWidth/2, displayHeight/2)
+end
+
+def setup
+  colorMode HSB, 360, 100, 100, 100
+  stroke_width 1
+  frame_rate 50
+  background 0
+  @center = Vector.elements([@width/2.0, @height/2.0])
+end
+
+def set_vars
+	tau, sig = rand, rand
+	while (sig + tau > 1)
+		sig = rand
 	end
+	[sig, tau]
+end
 
+def barycentric
+	sig, tau = set_vars
+	pt = A + sig * BA + tau * CA + sig*tau*A
+	rPt = rescale(pt)
+	ellipse(*rPt, 1, 1)
+end
 
+def rescale(pt)
+	50 * pt + @center
+end
 
-
-	def draw
-
-
-	end
+def draw
+  stroke (rand 360), 60, 100, 100
+	barycentric
+end
