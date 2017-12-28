ec9318b392920414a3a1beba1af5796738df311d
Jon Zingale <jonzingale@gmail.com>
2017-12-28 13:07:21 -0700

    toward visualizing the triangles problem

---
+++ triangles_projectEuler102.rb
@@ -0,0 +1,20 @@
+  def settings
+    size(displayWidth, displayHeight)
+  end
+
+	def setup
+		@w, @h = [width/2.0, 0]
+		@i = 0 ; @t = 0
+    frame_rate 20
+
+		colorMode(HSB,360,100,100,100)
+		background(0)
+	end
+
+
+
+
+	def draw
+
+
+	end
