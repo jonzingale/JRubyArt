a7e71d12b34b5f0c461e7f5a6306d64e48997712
Jon Zingale <jonzingale@gmail.com>
2019-07-03 13:44:50 -0400

    better spacing, fences, todo

--- my_vacation/fireflies_on_cook.rb
+++ my_vacation/fireflies_on_cook.rb
@@ -1,23 +1,22 @@
   URL_STUB = '/Users/Jon/Desktop/crude/RubyProcessing/my_vacation/'
 
   class FireFly
-    def initialize(width, height)
-      @w, @h = width, height
+    def initialize(ww, hh)
       @pert = 3*rand/5 + 3/5.0
-      render_body
+      @w = ww * rand
+      @h = hh * rand/2.0
     end
 
-    def render_body
-      no_stroke
-      # vibrancy of body light
-      if rand < 0.99
-        fill(0,0,0,60)
-      else
-        fill(68, 100*@pert, 100*@pert)
-        # write code to raise lights
-      end
+    # todo:
+    # rising fireflies
+    # house, car, tree
+    # blinky grass?
 
-      ellipse(@w-60, @h-30, 10, 10) # body
+    def render_body
+      # vibrancy of body light
+      rand < 0.99 ? fill(0,0,0,60) : fill(68, 100*@pert, 100*@pert)
+        # write code to raise lights
+      ellipse(@w, @h, 10, 10) # body
     end
   end
 
@@ -27,18 +26,25 @@
     end
 
     def render_yard
-      stroke_width 40
-      # sidewalk
-      stroke(30,20,30); line(0, 80*height/100, width, 73*height/100)
-      fill(30,20,30); rect(0, 80*height/100, width, 300)
-
-      stroke_width 0.2
-      # grass
-      stroke(100,255,30); line(0, 65*height/100, width, 58*height/100)      
-      fill(100,255,30); rect(0, 65*height/100, width, 30)
-
       # fence
-      stroke(30,0,60); line(300, 50*height/100, width, 43*height/100)
+      stroke(30,0,60,30); stroke_width(0.1)
+      (0..8).each do |i|
+        line(300, (50+2*i)*height/100, width, (43+2*i)*height/100)
+      end ; no_stroke
+
+      # grass
+      fill(100,255,30);
+      quad(0, 65*height/100,
+           width, 58*height/100,
+           width, height,
+           0, height)
+
+      # sidewalk
+      fill(30,20,55);
+      quad(0, 80*height/100,
+           width, 73*height/100,
+           width, height,
+           0, height)
 
     end
   end
@@ -50,16 +56,13 @@
     frame_rate 5 ;
     background 0
     colorMode(HSB,360,100,100,100)
-    text_font create_font("times", 30)
-    @firefly = [*1..50].map{ |i| FireFly.new(rand*width, rand*height) }
+    text_font create_font("word", 25)
+    @firefly = (1..80).map{ |i| FireFly.new(width, height) }
     @yard = Yard.new
-    fill(0,0,100) ; text('My Lakewood Vacation', 10, height/10)
-    # @img = loadImage(URL_STUB+'beach.jpg')
   end
 
   def draw
-    # image(@img, 0, 0)
-    @firefly.each{|fly| fly.render_body}
-    fill(220,20,100) ; text('My Lakewood Vacation', 10, height/10)
     @yard.render_yard
+    @firefly.each{|fly| fly.render_body}
+    fill(220,20,100) ; text('Fireflies on Cook Avenue', 20, 9*height/10)
   end
