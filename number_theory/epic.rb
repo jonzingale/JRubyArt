81ed34a5a3600b648851651f8453ced7135ff0cc
Jon Zingale <jonzingale@gmail.com>
2019-10-07 19:21:01 -0600

    working grid and abstracted math epic class

---
+++ number_theory/epic.rb
@@ -0,0 +1,30 @@
+# require 'byebug'
+
+class Epic
+  def evalPair(fsym, x, y)
+    self.send(fsym, x, y)
+  end
+
+  def fact(n)
+    (1..n).inject(1, :*)
+  end
+
+  def choose(n, k)
+    fact(n) / (fact(k) * fact(n-k))
+  end
+
+  def epis(d, c)
+    (0..c).inject(0) {|a, k| a + (-1)**k * choose(c,k) * (c-k)**d }
+  end
+end
+
+def grid(size)
+  [*0..size].product([*0..size])
+end
+
+# it = Epic.new
+# that = it.evalPair(:epis, 4, 3)
+# puts that
+
+# byebug
+# 3
--- number_theory/epimorphisms.rb
+++ number_theory/epimorphisms.rb
@@ -1,10 +1,31 @@
+require_relative 'epic'
+
+class Grid
+  attr_accessor :coords
+  def initialize(size, width, height)
+    @size = size
+    @coords = [*0..size].product([*0..size])
+    @x_int = width/size
+    @y_int = height/size
+    @maths = Epic.new
+  end
+
+  def to_grid
+    @coords.map do |x, y|
+      val = @maths.epis(x+1, y+1)
+      rect = [x*@x_int, y*@y_int, x*@x_int + @size, y*@y_int + @size]
+      [val, rect]
+    end
+  end
+end
+
 def settings ; size(500, 500) end
 
 def setup
-  frame_rate 5 ;
+  frame_rate 1;
   background 0
   colorMode(HSB,360,100,100,100)
-  text_font create_font("word", 25)
+  @grid = Grid.new(20, @width, @height)
 end
 
 # Todo:
@@ -15,19 +36,11 @@ end
 # test the same for factorial instead of number of maps.
 
 def draw
-  fill(200,100,100,100)
-  rect(0,0,width,height)
+  them = @grid.to_grid
+  them.each do |(val, rec)|
+    fill(val % 360,val % 100,val % 10 * 10, 100)
+    rect(*rec)
+  end
 end
 
-def fact(n)
-  (1..n).inject(1, :*)
-end
-
-def choose(n, k)
-  fact(n) / (fact(k) * fact(n-k))
-end
-
-def epis(d, c)
-  (0..c).inject(0) {|a, k| a + (-1)**k * choose(c,k) * (c-k)**d }
-end
 
