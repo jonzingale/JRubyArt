7f8ef26f59486569d4c4cd7ac6d178c45c141b8c
Jon Zingale <jonzingale@gmail.com>
2019-10-08 00:07:18 -0600

    towards correct data

--- number_theory/epimorphisms.rb
+++ number_theory/epimorphisms.rb
@@ -5,27 +5,26 @@ class Grid
   def initialize(size, width, height)
     @size = size
     @coords = [*0..size].product([*0..size])
-    @x_int = width/size
-    @y_int = height/size
+    @x_int, @y_int = [width/size, height/size]
     @maths = Epic.new
   end
 
   def to_grid
     @coords.map do |x, y|
-      val = @maths.epis(x+1, y+1)
-      rect = [x*@x_int, y*@y_int, x*@x_int + @size, y*@y_int + @size]
+      val = @maths.epis(x, y)
+      rect = [x*@x_int, y*@y_int, x*(@x_int + @size), y*(@y_int + @size)]
       [val, rect]
     end
   end
 end
 
-def settings ; size(500, 500) end
+def settings ; size(1800, 1000) end
 
 def setup
   frame_rate 1;
   background 0
   colorMode(HSB,360,100,100,100)
-  @grid = Grid.new(20, @width, @height)
+  @grid = Grid.new(100, @width, @height)
 end
 
 # Todo:
@@ -38,9 +37,12 @@ end
 def draw
   them = @grid.to_grid
   them.each do |(val, rec)|
-    fill(val % 360,val % 100,val % 10 * 10, 100)
+    if val == 0
+      fill(val % 100,100,0,0)
+    else
+      fill(val % 360,val % 100,val % 100,100)
+      # fill(val % 100,100,100,100)
+    end
     rect(*rec)
   end
 end
-
-
